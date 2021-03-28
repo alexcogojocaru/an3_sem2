b4fd892383d441db0f8c5e98070f9839f627d8ea
Alex Cojocaru <alex.cojocaru73@gmail.com>
2021-03-28 18:25:53 +0300

    has the user changed

---
+++ spg/opengl/opengl.cpp
@@ -0,0 +1,21 @@
+﻿// opengl.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+using namespace std;
+
+int main()
+{
+	GLFWwindow* window = glfwCreateWindow(600, 600, "spg", nullptr, nullptr);
+
+	return 0;
+}
